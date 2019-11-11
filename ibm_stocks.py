72035f414c27778b353c1218cbf31720c742321e
itsirovasilis <ioannistsirovasilis@gmail.com>
2019-11-12 00:03:34 +0200

    - Added pandas to handle csv files - Restructured project - Added requirements.txt

--- ibm_stocks.py
+++ ibm_stocks.py
@@ -1,13 +1,18 @@
+import pandas
+
+FILE_PATH = "files/IBM.csv"
+OUTPUT_FILE_PATH = "files/results.csv"
 
 
-with open("files/IBM.csv", "r") as f:
-    content = f.readlines()
+def calculate_daily_stock_variance(open_value: float, close_value: float):
+    return 100 * (open_value - close_value) / open_value
 
-new_content = []
-with open("files/results.csv", "w") as f:
-    print("writing lines...")
-    new_content = [c.replace("\n", "") + "," + str(100 * (float(c.split(",")[1]) - float(c.split(",")[4])) /
-                   float(c.split(",")[1])) + "\n" for c in content if c != content[0]]
-    f.write("Date,Open,High,Low,Close,Adj Close,Volume,Daily Stock\n")
-    f.writelines(new_content)
 
+def main():
+    content = pandas.read_csv(FILE_PATH)
+    content["Daily Stock"] = 100 * (content["Open"] - content["Close"]) / content["Open"]
+    content.to_csv(OUTPUT_FILE_PATH)
+
+
+if __name__ == "__main__":
+    main()
