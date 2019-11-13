ce695a19a4953f3e9383b403cee2b37f39877eb6
TzavarasIoannis <tzavaras95@gmail.com>
2019-11-13 17:06:45 +0200

    Adding plots

--- ibm_stocks.py
+++ ibm_stocks.py
@@ -1,4 +1,9 @@
 import pandas
+import matplotlib.pyplot as plt
+import seaborn as sn
+import numpy as np
+from scipy.stats import norm
+
 
 FILE_PATH = "files/IBM.csv"
 OUTPUT_FILE_PATH = "files/results.csv"
@@ -14,5 +19,38 @@ def main():
     content.to_csv(OUTPUT_FILE_PATH)
 
 
+    
+
+    Daily_stock = list(content.iloc[:,7])
+    Daily_stock_freq = {}
+    for val in Daily_stock:
+      Daily_stock_freq[ val ] = Daily_stock.count(val)
+
+    
+    plt.bar(Daily_stock_freq.keys() , Daily_stock_freq.values(), width=0.8)
+    plt.xlim(-15,25)
+    plt.xticks(np.arange(-15,25,1))
+    plt.xlabel("Daily Stocks")
+    plt.ylabel("Amount")
+    plt.grid(True)
+    plt.show() 
+    
+    mean,std = norm.fit(Daily_stock)
+  
+    sn.distplot( content.iloc[:,7], hist=True , rug=True )
+    plt.xlim(-15,25)
+    x= np.linspace(-15,25,100)
+    y=norm.pdf(x,mean,std)
+    plt.plot(x,y, color ='r')
+    plt.xlabel('Daily stocks')
+    plt.ylabel('Frequency')
+    plt.text(-2,0.45,r"Gaussian fit on Stocks")
+ 
+    #min, max values :
+    min_stock = np.min(Daily_stock)
+    max_stock = np.max(Daily_stock)
+    
+    
+    
 if __name__ == "__main__":
     main()
